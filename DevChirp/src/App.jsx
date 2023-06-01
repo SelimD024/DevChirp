a301f00f7723b8023d5c0e788ace8c5368dbc41b
Stijn ter haar <stijn.ter.haar2004@gmail.com>
2023-06-01 12:37:37 +0200

    NEW VERSION

---
+++ DevChirp/src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import burger from "./assets/burger.svg";
+import avatar from "./assets/avatar.svg";
+import "./App.css";
+import headerbackground from "./assets/headerbackground.png";
+import cardimage from "./assets/cardimage.png";
+import Clogo from "./assets/Clogo.png";
+import javascriptimage from "./assets/javascript.png";
+import javalogo from "./assets/javascriptlogo.png";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <main className="">
+        <div
+          className="header"
+          style={{ backgroundImage: `url(${headerbackground})` }}
+        >
+          <div className="top">
+            <img src={burger} alt="React Logo" />
+            <div className="navigation">
+              <ul>
+                <li>Home</li>
+                <li>Topics</li>
+              </ul>
+            </div>
+            <img src={avatar} alt="React Logo" />
+          </div>
+          <div className="middle">
+            <h1>Hi, Stijn</h1>
+            <p>Find community by using topics or products</p>
+          </div>
+          <div className="bottom">
+            <div className="searchbar"></div>
+          </div>
+        </div>
+        <div className="community-cards">
+          <h1>Top Communities</h1>
+          <div className="cards">
+            <div className="card">
+              <div
+                className="top"
+                style={{ backgroundImage: `url(${cardimage})` }}
+              >
+                <div
+                  className="card-icon"
+                  style={{ backgroundImage: `url(${Clogo})` }}
+                ></div>
+              </div>
+              <div className="bottom">
+                <h2>C#</h2>
+                <p>23.600 members</p>
+              </div>
+            </div>
+            <div className="card">
+              <div
+                className="top"
+                style={{ backgroundImage: `url(${javascriptimage})` }}
+              >
+                <div
+                  className="card-icon"
+                  style={{ backgroundImage: `url(${javalogo})` }}
+                ></div>
+              </div>
+              <div className="bottom">
+                <h2>Javascript</h2>
+                <p>23.600 members</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="community-cards">
+          <h1>Design Communities</h1>
+          <div className="cards">
+            <div className="card">
+              <div
+                className="top"
+                style={{ backgroundImage: `url(${cardimage})` }}
+              >
+                <div
+                  className="card-icon"
+                  style={{ backgroundImage: `url(${Clogo})` }}
+                ></div>
+              </div>
+              <div className="bottom">
+                <h2>C#</h2>
+                <p>23.600 members</p>
+              </div>
+            </div>
+            <div className="card">
+              <div
+                className="top"
+                style={{ backgroundImage: `url(${cardimage})` }}
+              >
+                <div
+                  className="card-icon"
+                  style={{ backgroundImage: `url(${Clogo})` }}
+                ></div>
+              </div>
+              <div className="bottom">
+                <h2>C#</h2>
+                <p>23.600 members</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
