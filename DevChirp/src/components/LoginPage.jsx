eaeb7174b7d671f38034aca0cb178ef4ea51e45c
Stijn ter haar <stijn.ter.haar2004@gmail.com>
2023-06-12 11:58:47 +0200

    Login update

--- DevChirp/src/components/LoginPage.jsx
+++ DevChirp/src/components/LoginPage.jsx
@@ -1,94 +1,23 @@
-import { useState } from "react";
-import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword, // Add this import
-} from "firebase/auth";
-import { initFirebase } from "../../firebase/firebase";
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { initFirebase } from '../../firebase/firebase';
 
 const Home = () => {
-  initFirebase();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isRegistering, setIsRegistering] = useState(false);
-
+    initFirebase
+  const provider = new GoogleAuthProvider();
   const auth = getAuth();
 
-  const handleGoogleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log(user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const handleEmailSignIn = async () => {
-    try {
-      if (isRegistering) {
-        const userCredential = await createUserWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        const user = userCredential.user;
-        console.log(user);
-      } else {
-        const userCredential = await signInWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        const user = userCredential.user;
-        console.log(user);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
+  const signIn = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result.user);
   };
 
   return (
-    <div className="login-form">
-      <div>Login to continue</div>
-      <button onClick={handleGoogleSignIn}>
-        <div className="bg-red-600 text-white rounded-md p-2 w-48">
-          Sign in with Google
-        </div>
+    <div className="text-center flex flex-col gap-4 items-center">
+      <div>login om door te gaan</div>
+      <button onClick={signIn}>
+        <div className="bg-red-600 text-white rounded-md p-2 w-48">Sign in</div>
       </button>
-      <div>or</div>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleEmailSignIn}>
-        <div className="bg-red-600 text-white rounded-md p-2 w-48">
-          {isRegistering ? "Register" : "Sign in"} with Email/Password
-        </div>
-      </button>
-      <div>
-        <label htmlFor="register-checkbox">
-          Register instead of signing in
-        </label>
-        <input
-          id="register-checkbox"
-          type="checkbox"
-          checked={isRegistering}
-          onChange={() => setIsRegistering(!isRegistering)}
-        />
-      </div>
     </div>
   );
 };
